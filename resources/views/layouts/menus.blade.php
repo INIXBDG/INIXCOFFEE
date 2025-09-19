01d6b66bf8259c3afde971f03156cb1f7fe55b55
Unsworthy <rshogun019@gmail.com>
2025-09-19 10:23:45 +0700

    Update: create exam only ft connect rkm and assign ruangan

--- resources/views/layouts/menus.blade.php
+++ resources/views/layouts/menus.blade.php
@@ -1658,6 +1658,25 @@
                                                         </div>
                                                     </div>
                                                      @endcan
+                                                     @can('View Management Kelas')
+                                                    <div class="col-sm-6 mt-2">
+                                                        <div class="card" id="card-hover">
+                                                            <div class="card-body d-flex">
+                                                                <div class="col-md-2">
+                                                                    <img src="{{ asset('icon/stats.svg') }}"
+                                                                        class="img-responsive" width="30px">
+                                                                </div>
+                                                                <div class="col-md-10" style="margin-left: 10px">
+                                                                    <a href="/managemetkelas"
+                                                                        class="link stretched-link text-decoration-none">
+                                                                        <h5 class="card-title">Management Kelas</h5>
+                                                                    </a>
+                                                                    <p class="card-text">Management Kelas RKM In-House Minggu Ini</p>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    @endcan
                                                 </div>
                                             </div>
                                         </div>
