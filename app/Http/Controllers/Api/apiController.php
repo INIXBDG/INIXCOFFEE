c793d19744e8095a3ce32f63a1c3dae7705125ab
ardhanshahab <ardhanshahab@gmail.com>
2026-03-04 14:07:11 +0700

    Penambahan feedback driver

--- app/Http/Controllers/Api/apiController.php
+++ app/Http/Controllers/Api/apiController.php
@@ -39,114 +39,73 @@ class apiController extends Controller
         $averageFeedbacks = [];
 
         foreach ($groupedFeedbacks as $materi_key => $feedbackGroup) {
-            $materi_key = $feedbackGroup->first()->rkm->materi_key;
-            $nama_materi = $feedbackGroup->first()->rkm->materi->nama_materi;
-            $instruktur_key = $feedbackGroup->first()->rkm->instruktur_key;
-            $sales_key = $feedbackGroup->first()->rkm->sales_key;
-            $created_at = $feedbackGroup->first()->created_at;
-            $tanggal_awal = Carbon::parse($feedbackGroup->first()->rkm->tanggal_awal);
-            $tanggal_awal = $tanggal_awal->format('Y-m-d');
-            // $bulan = $tanggal_awal->format('m');
-            $tanggal_akhir = $feedbackGroup->first()->rkm->tanggal_akhir;
+
+            $first = $feedbackGroup->first();
+
+            $materi_key     = $first->rkm->materi_key;
+            $nama_materi    = $first->rkm->materi->nama_materi;
+            $instruktur_key = $first->rkm->instruktur_key;
+            $sales_key      = $first->rkm->sales_key;
+            $created_at     = $first->created_at;
+
+            $tanggal_awal = Carbon::parse($first->rkm->tanggal_awal)->format('Y-m-d');
+            $tanggal_akhir = $first->rkm->tanggal_akhir;
+
             $totalFeedbacks = $feedbackGroup->count();
-            $totalM1 = $feedbackGroup->sum('M1');
-            $totalM2 = $feedbackGroup->sum('M2');
-            $totalM3 = $feedbackGroup->sum('M3');
-            $totalM4 = $feedbackGroup->sum('M4');
-            $totalP1 = $feedbackGroup->sum('P1');
-            $totalP2 = $feedbackGroup->sum('P2');
-            $totalP3 = $feedbackGroup->sum('P3');
-            $totalP4 = $feedbackGroup->sum('P4');
-            $totalP5 = $feedbackGroup->sum('P5');
-            $totalP6 = $feedbackGroup->sum('P6');
-            $totalP7 = $feedbackGroup->sum('P7');
-            $totalF1 = $feedbackGroup->sum('F1');
-            $totalF2 = $feedbackGroup->sum('F2');
-            $totalF3 = $feedbackGroup->sum('F3');
-            $totalF4 = $feedbackGroup->sum('F4');
-            $totalF5 = $feedbackGroup->sum('F5');
-            $totalI1 = $feedbackGroup->sum('I1');
-            $totalI2 = $feedbackGroup->sum('I2');
-            $totalI3 = $feedbackGroup->sum('I3');
-            $totalI4 = $feedbackGroup->sum('I4');
-            $totalI5 = $feedbackGroup->sum('I5');
-            $totalI6 = $feedbackGroup->sum('I6');
-            $totalI7 = $feedbackGroup->sum('I7');
-            $totalI8 = $feedbackGroup->sum('I8');
-            $totalI1b = $feedbackGroup->sum('I1b');
-            $totalI2b = $feedbackGroup->sum('I2b');
-            $totalI3b = $feedbackGroup->sum('I3b');
-            $totalI4b = $feedbackGroup->sum('I4b');
-            $totalI5b = $feedbackGroup->sum('I5b');
-            $totalI6b = $feedbackGroup->sum('I6b');
-            $totalI7b = $feedbackGroup->sum('I7b');
-            $totalI8b = $feedbackGroup->sum('I8b');
-            $totalI1as = $feedbackGroup->sum('I1as');
-            $totalI2as = $feedbackGroup->sum('I2as');
-            $totalI3as = $feedbackGroup->sum('I3as');
-            $totalI4as = $feedbackGroup->sum('I4as');
-            $totalI5as = $feedbackGroup->sum('I5as');
-            $totalI6as = $feedbackGroup->sum('I6as');
-            $totalI7as = $feedbackGroup->sum('I7as');
-            $totalI8as = $feedbackGroup->sum('I8as');
 
-
-            $averageFeedbacks[] = [
-                'nama_materi' => $nama_materi,
-                'materi_key' => $materi_key,
-                'instruktur_key' => $instruktur_key,
-                // 'bulan' => $bulan,
-                'sales_key' => $sales_key,
-                'tanggal_awal' => $tanggal_awal,
-                'tanggal_akhir' => $tanggal_akhir,
-                'created_at' => $created_at,
-                'averageM1' => $totalM1 / $totalFeedbacks,
-                'averageM2' => $totalM2 / $totalFeedbacks,
-                'averageM3' => $totalM3 / $totalFeedbacks,
-                'averageM4' => $totalM4 / $totalFeedbacks,
-                'averageP1' => $totalP1 / $totalFeedbacks,
-                'averageP2' => $totalP2 / $totalFeedbacks,
-                'averageP3' => $totalP3 / $totalFeedbacks,
-                'averageP4' => $totalP4 / $totalFeedbacks,
-                'averageP5' => $totalP5 / $totalFeedbacks,
-                'averageP6' => $totalP6 / $totalFeedbacks,
-                'averageP7' => $totalP7 / $totalFeedbacks,
-                'averageF1' => $totalF1 / $totalFeedbacks,
-                'averageF2' => $totalF2 / $totalFeedbacks,
-                'averageF3' => $totalF3 / $totalFeedbacks,
-                'averageF4' => $totalF4 / $totalFeedbacks,
-                'averageF5' => $totalF5 / $totalFeedbacks,
-                'averageI1' => $totalI1 / $totalFeedbacks,
-                'averageI2' => $totalI2 / $totalFeedbacks,
-                'averageI3' => $totalI3 / $totalFeedbacks,
-                'averageI4' => $totalI4 / $totalFeedbacks,
-                'averageI5' => $totalI5 / $totalFeedbacks,
-                'averageI6' => $totalI6 / $totalFeedbacks,
-                'averageI7' => $totalI7 / $totalFeedbacks,
-                'averageI8' => $totalI8 / $totalFeedbacks,
-                'averageI1b' => $totalI1b / $totalFeedbacks,
-                'averageI2b' => $totalI2b / $totalFeedbacks,
-                'averageI3b' => $totalI3b / $totalFeedbacks,
-                'averageI4b' => $totalI4b / $totalFeedbacks,
-                'averageI5b' => $totalI5b / $totalFeedbacks,
-                'averageI6b' => $totalI6b / $totalFeedbacks,
-                'averageI7b' => $totalI7b / $totalFeedbacks,
-                'averageI8b' => $totalI8b / $totalFeedbacks,
-                'averageI1as' => $totalI1as / $totalFeedbacks,
-                'averageI2as' => $totalI2as / $totalFeedbacks,
-                'averageI3as' => $totalI3as / $totalFeedbacks,
-                'averageI4as' => $totalI4as / $totalFeedbacks,
-                'averageI5as' => $totalI5as / $totalFeedbacks,
-                'averageI6as' => $totalI6as / $totalFeedbacks,
-                'averageI7as' => $totalI7as / $totalFeedbacks,
-                'averageI8as' => $totalI8as / $totalFeedbacks,
-                'averageM' => round(($totalM1 + $totalM2 + $totalM3 + $totalM4) / ($totalFeedbacks * 4), 1),
-                'averageP' => round(($totalP1 + $totalP2 + $totalP3 + $totalP4 + $totalP5 + $totalP6 + $totalP7) / ($totalFeedbacks * 7), 1),
-                'averageF' => round(($totalF1 + $totalF2 + $totalF3 + $totalF4 + $totalF5) / ($totalFeedbacks * 5), 1),
-                'averageI' => round(($totalI1 + $totalI2 + $totalI3 + $totalI4 + $totalI5 + $totalI6 + $totalI7 + $totalI8) / ($totalFeedbacks * 8), 1),
-                'averageIb' => round(($totalI1b + $totalI2b + $totalI3b + $totalI4b + $totalI5b + $totalI6b + $totalI7b + $totalI8b) / ($totalFeedbacks * 8), 1),
-                'averageIas' => round(($totalI1as + $totalI2as + $totalI3as + $totalI4as + $totalI5as + $totalI6as + $totalI7as + $totalI8as) / ($totalFeedbacks * 8), 1),
+            // =========================
+            // FIELD GROUP DEFINISI
+            // =========================
+            $groups = [
+                'M'   => ['M1','M2','M3','M4'],
+                'P'   => ['P1','P2','P3','P4','P5','P6','P7','P8'], // <-- tinggal tambah sini kalau ada P9
+                'F'   => ['F1','F2','F3','F4','F5'],
+                'I'   => ['I1','I2','I3','I4','I5','I6','I7','I8'],
+                'Ib'  => ['I1b','I2b','I3b','I4b','I5b','I6b','I7b','I8b'],
+                'Ias' => ['I1as','I2as','I3as','I4as','I5as','I6as','I7as','I8as'],
             ];
+
+            $result = [];
+
+            foreach ($groups as $groupName => $fields) {
+
+                $totalGroupScore = 0;
+                $activeFieldCount = 0;
+
+                foreach ($fields as $field) {
+
+                    // cek apakah kolom ada & ada nilainya
+                    if ($feedbackGroup->whereNotNull($field)->count() > 0) {
+
+                        $fieldTotal = $feedbackGroup->sum(function ($item) use ($field) {
+                            return $item->$field ?? 0;
+                        });
+
+                        $result["average{$field}"] = $totalFeedbacks > 0
+                            ? $fieldTotal / $totalFeedbacks
+                            : 0;
+
+                        $totalGroupScore += $fieldTotal;
+                        $activeFieldCount++;
+                    }
+                }
+
+                // Hitung rata-rata group (M, P, F, dst)
+                $result["average{$groupName}"] =
+                    ($totalFeedbacks > 0 && $activeFieldCount > 0)
+                    ? round($totalGroupScore / ($totalFeedbacks * $activeFieldCount), 1)
+                    : 0;
+            }
+
+            $averageFeedbacks[] = array_merge([
+                'nama_materi'    => $nama_materi,
+                'materi_key'     => $materi_key,
+                'instruktur_key' => $instruktur_key,
+                'sales_key'      => $sales_key,
+                'tanggal_awal'   => $tanggal_awal,
+                'tanggal_akhir'  => $tanggal_akhir,
+                'created_at'     => $created_at,
+            ], $result);
         }
 
         // Urutkan hasil berdasarkan tanggal_awal secara descending
