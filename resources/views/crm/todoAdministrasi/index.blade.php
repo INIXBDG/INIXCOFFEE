79593a37ee69db204ad3d96ee4f359097a9baa3a
Jonathan Valentino Aditya <valentino13020@gmail.com>
2026-03-31 19:55:44 -0700

    Add pagination to todo list view
    Signed-off-by: Jonathan Valentino Aditya <valentino13020@gmail.com>

--- resources/views/crm/todoAdministrasi/index.blade.php
+++ resources/views/crm/todoAdministrasi/index.blade.php
@@ -171,6 +171,11 @@
                     </tbody>
                 </table>
             </div>
+            
+            <!-- Pagination -->
+            <div class="mt-4 d-flex justify-content-center">
+                {{ $todos->links() }}
+            </div>
         </div>
     </div>
 
