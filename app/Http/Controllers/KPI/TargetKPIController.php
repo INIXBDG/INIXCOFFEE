4188a909ee616c87823543a3a54ae0c666eac6ad
Jonathan Valentino <valentino13020@gmail.com>
2026-04-24 10:01:20 +0700

    Perbaikan function kpi konsistensi campaign digital dan form kpi gap kompetensi

--- app/Http/Controllers/KPI/TargetKPIController.php
+++ app/Http/Controllers/KPI/TargetKPIController.php
@@ -7858,7 +7858,7 @@ class TargetKPIController extends Controller
     }
 
     //Tim Digital
-    private function calculateKonsistensiHybridDetail($itemDetail)
+    private function calculateKonsistensiCampaignDigitalDetail($itemDetail)
 	{
 		$details = $itemDetail->detailTargetKPI;
 
