ccb35abff48e039093a5d8a1f288e15e719e9597
SergioMosesR <141124656+SergioMosesR@users.noreply.github.com>
2026-07-03 17:13:20 +0700

    Add 'AI' option to kategori_materi select
    Signed-off-by: SergioMosesR <141124656+SergioMosesR@users.noreply.github.com>

--- resources/views/materi/edit.blade.php
+++ resources/views/materi/edit.blade.php
@@ -44,6 +44,7 @@
                                     <option @if ($materis->kategori_materi == "Software Engineer") selected @endif value="Software Engineer">Software Engineer</option>
                                     <option @if ($materis->kategori_materi == "Office") selected @endif value="Office">Office</option>
                                     <option @if ($materis->kategori_materi == "Non-IT") selected @endif value="Non-IT">Non-IT</option>
+                                    <option @if ($materis->kategori_materi == "AI") selected @endif value="AI">AI</option>
                                 </select>
                                 @error('kategori_materi')
                                     <span class="invalid-feedback" role="alert">
