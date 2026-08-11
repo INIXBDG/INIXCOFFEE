391cab10ff98dac15c3b090481a56d6ec1b783fb
SergioMosesR <sergiomoses.r@gmail.com>
2026-08-11 14:57:34 +0700

    Update query

--- app/Http/Controllers/Api/RKMController.php
+++ app/Http/Controllers/Api/RKMController.php
@@ -52,7 +52,7 @@ class RKMController extends Controller
                     ->join('materis', 'r_k_m_s.materi_key', '=', 'materis.id')
                     ->whereBetween('r_k_m_s.tanggal_awal', [$start, $end])
                     ->whereDoesntHave('peluang', function ($query) {
-                        $query->where('tentatif', 1);
+                        $query->where('tentatif', 1)->where('tahap', '!=', 'lost');
                     })
                     ->select(
                         DB::raw('GROUP_CONCAT(r_k_m_s.id SEPARATOR ", ") AS id'),
