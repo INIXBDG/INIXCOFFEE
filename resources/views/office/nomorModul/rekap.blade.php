8675a25ab4ba1d04c1139ff5572fc48e5406f4e0
SergioMosesR <sergiomoses.r@gmail.com>
2026-09-18 10:08:27 +0700

    Hide Skeleton

--- resources/views/office/nomorModul/rekap.blade.php
+++ resources/views/office/nomorModul/rekap.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts_office.app')
 
 @section('office_contents')
-    @include('layouts_office.skeleton_stats')
-    <div id="real-dashboard" class="container-fluid py-4 d-none">
+    {{-- @include('layouts_office.skeleton_stats') --}}
+    <div id="real-dashboard" class="container-fluid py-4">
         <div class="d-flex justify-content-between align-items-center mb-4">
             <h4 class="mb-0 fw-bold">Rekap Modul</h4>
         </div>
